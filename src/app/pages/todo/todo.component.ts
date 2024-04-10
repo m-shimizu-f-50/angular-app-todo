1ae0936f96f0b8ff2ed04a3546ef8d40e34a5201
simizu-masato <m.shimizu@trigger4.com>
2024-04-10 17:07:49 +0900

    Todo画面 ビジネスロジック雛形作成

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from 'src/app/services/todo/todo.service';
+import { Todo } from 'src/app/services/todo/todo';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  todoList: Todo[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/services/todo/todo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  todoList: Todo[] = [];
+
+  constructor() {}
+
+  /**
+   * 新規登録
+   */
+
+  /**
+   * 全件削除
+   */
+}
