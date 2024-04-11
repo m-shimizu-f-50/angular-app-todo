30ae451e4238d96f03b2e85910dd683a5904d2ff
simizu-masato <m.shimizu@trigger4.com>
2024-04-11 21:36:47 +0900

    新規登録デザイン修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     "./src/app/components/header/header.component.html",
     "./src/app/components/footer/footer.component.html",
     "./src/app/pages/not-found/not-found.component.html",
-    "./src/app/pages/todo/todo.component.html",
+    "./src/app/pages/todo/todo-list/todo-list.component.html",
+    "./src/app/pages/todo/todo-add/todo-add.component.html",
   ],
   theme: {
     extend: {},
