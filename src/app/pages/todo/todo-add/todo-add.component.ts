0756f0acc42fa990ea0c2cc995dd05dfa00afcc5
simizu-masato <m.shimizu@trigger4.com>
2024-04-12 22:09:00 +0900

    新規登録バリデーション実装

--- src/app/pages/todo/todo-add/todo-add.component.ts
+++ src/app/pages/todo/todo-add/todo-add.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  FormControl,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { TodoService } from 'src/app/services/todo/todo.service';
 import { Todo } from 'src/app/services/todo/todo';
 import { Router } from '@angular/router';
@@ -15,15 +10,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./todo-add.component.css'],
 })
 export class TodoAddComponent implements OnInit {
-  // todo: Todo = {
-  //   id: 0,
-  //   status: 0,
-  //   title: '',
-  //   description: '',
-  //   startDate: '',
-  //   endDate: '',
-  // };
-
   todoForm = new FormGroup({
     title: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
@@ -36,7 +22,6 @@ export class TodoAddComponent implements OnInit {
   ngOnInit(): void {}
 
   addTodo(): void {
-    console.log('OK');
     // フォームの値にidとstatusを追加して送信
     const newTodo: Todo = {
       id: 0,
@@ -47,7 +32,6 @@ export class TodoAddComponent implements OnInit {
       endDate: this.todoForm.value.endDate ?? '',
     };
     this.todoService.addToList(newTodo);
-    // this.todoService.addToList(Object.assign({}, this.todo));
     this.router.navigate(['/todo']);
   }
 }
